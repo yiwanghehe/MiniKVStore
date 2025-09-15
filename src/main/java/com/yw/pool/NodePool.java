96ff726434cdd0cdc132fa7d6e4e68b88eb29fe0
yiwanghehe <1243849362@qq.com>
2025-09-15 21:08:09 +0800

    修复NodePool设计不当导致的严重性能下降问题。在NodePool.release() 方法中，通过调用 ConcurrentLinkedQueue.size() 来检查池大小。这是一个 O(N) 操作，当池内对象增多时，每次归还节点都会遍历整个队列，导致刷盘和关闭引擎时性能急剧恶化。引入一个AtomicInteger变量来高效、线程安全地追踪池的大小。自身维护此原子计数器，时间复杂度只需O(1)

--- src/main/java/com/yw/pool/NodePool.java
+++ src/main/java/com/yw/pool/NodePool.java
@@ -3,10 +3,12 @@ package com.yw.pool;
 import com.yw.node.Node;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class NodePool <K extends Comparable <K>, V> {
     private final ConcurrentLinkedQueue<Node<K, V>> pool = new ConcurrentLinkedQueue<>();
-    private static final int MAX_POOL_SIZE = 1000000; // 设置池上限，防止内存无限增长
+    private final AtomicInteger poolSize = new AtomicInteger(0); // 原子计数器来追踪池的大小
+    private static final int MAX_POOL_SIZE = 100000; // 设置池上限，防止内存无限增长
 
     /**
      * 从池中获取一个节点。如果池为空，则创建一个新节点。
@@ -20,8 +22,8 @@ public class NodePool <K extends Comparable <K>, V> {
         Node<K, V> node = pool.poll();
         if (node != null) {
             // 如果从池中获取到节点，则重置其状态
+            poolSize.decrementAndGet(); // 池中出队节点，计数器-1
             node.reset(key, value, level);
-//            System.out.println("从对象池中创建对象");
             return node;
         }
         // 池为空，创建一个新节点
@@ -34,9 +36,11 @@ public class NodePool <K extends Comparable <K>, V> {
      * @param node 要释放的节点
      */
     public void release(Node<K, V> node) {
-        if (pool.size() < MAX_POOL_SIZE) {
+        // 不使用ConcurrentLinkedQueue里的size()来精确判断对象池大小，内置size()性能极差，时间复杂度O(N)
+        // 自身维护一个高效的原子计数器来进行检查
+        if (poolSize.get() < MAX_POOL_SIZE) {
             pool.offer(node);
-//            System.out.println("归还节点");
+            poolSize.incrementAndGet(); // 放入池中，计数器+1
         }
         // 如果池已满，则不添加，让GC回收该节点
     }
